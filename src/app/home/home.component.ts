2f8957b300b9baf477ec96b1f5a4919800a9364c
Šime Pavlić <pavlicsime1994@gmail.com>
2018-07-15 16:05:28 +0200

    Second Assignment rectified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
 
   dish: Dish;
   promotion: Promotion;
+  leader: Leader;
 
   constructor(private dishservice: DishService, private promotionservice: PromotionService, private leaderservice: LeaderService) { }
 
