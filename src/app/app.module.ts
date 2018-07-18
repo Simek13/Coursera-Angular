4d6c29fa95e05377423cf74b81ab361278e310b0
Šime Pavlić <pavlicsime1994@gmail.com>
2018-07-18 20:47:05 +0200

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import 'hammerjs';
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -52,7 +53,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
 
